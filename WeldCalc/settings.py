a20b639be57c34d10aa6c86a2802f3d9953c34aa
Gleb Dovzhenko <dovjenko.g@gmail.com>
2022-04-15 18:45:40 +0700

    Added dev server [2]

--- WeldCalc/settings.py
+++ WeldCalc/settings.py
@@ -24,12 +24,12 @@ with open('secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '.localhost',
-    '127.0.0.1',
-    '172.17.2.31'
+    # '.localhost',
+    # '127.0.0.1',
+    # '172.17.2.31'
 ]
 
 
